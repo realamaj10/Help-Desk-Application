79c1eafd9ab6b20db32d3d6f7fda7893b55c5779
Rea Lamaj <realamaj10@gmail.com>
2024-05-24 12:39:41 +0200

    First commit

---
+++ HelpDesk Project/src/main/java/com/app/IUser.java
@@ -0,0 +1,8 @@
+package com.app;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public interface IUser extends CrudRepository<User, Long> {
+
+}
